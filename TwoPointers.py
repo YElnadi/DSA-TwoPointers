b07163ed714cc5c8ed2d0baf7b8cc4b43284a760
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-08-11 00:35:26 -0700

    done

--- TwoPointers.py
+++ TwoPointers.py
@@ -2,3 +2,21 @@
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
+def isPalindrome(s):
+        s_lst = ''.join(s.split()).lower()
+        print(s_lst)
+        new_str = ''
+        print(s_lst)
+        for char in s_lst:
+                if char.isalnum():
+                        new_str=new_str+char
+        
+        print()
+        reversed_str = new_str[::-1]
+        return new_str == reversed_str
+                
+
+
+
+
+print(isPalindrome("0P"))
