b2fb280267ebb2343cb42c4ca63325998417c382
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-08-13 23:41:31 -0700

    done

--- binaryTree/invert-binarytree.py
+++ binaryTree/invert-binarytree.py
@@ -1,8 +1,11 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, right=None, left=None):
         self.val = val
-        self.right= right
-        self.left= left
+        self.right = right
+        self.left = left
 
 
 a = TreeNode(4)
@@ -21,10 +24,26 @@ c.left = f
 c.right = g
 
 
-
 #        4
 #      /   \
 #     2     7
-#   /  \    / \  
+#   /  \    / \
 #  1    3   6  9
+
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.right)
+                queue.append(node.left)
+
+        return root
+
 
